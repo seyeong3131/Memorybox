ca9b0fdabd7d412cb5e8d762343211f3cbe86b56
seyeong3131 <tpdud3131@naver.com>
2022-08-05 11:28:47 +0900

    파일 생성

---
+++ memorybox/src/main/java/com/memorybox/MemoryboxApplication.java
@@ -0,0 +1,13 @@
+package com.memorybox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryboxApplication.class, args);
+	}
+
+}
---
+++ memorybox/src/test/java/com/memorybox/MemoryboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memorybox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
