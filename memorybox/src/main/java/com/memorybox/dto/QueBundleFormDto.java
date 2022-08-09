9f745ce9d6eea8cf4fad179c9a00b1626c05822a
seyeong <tpdud3131@naver.com>
2022-08-09 16:49:03 +0900

    마스터 브랜치 병합

---
+++ memorybox/src/main/java/com/memorybox/dto/QueBundleFormDto.java
@@ -0,0 +1,32 @@
+package com.memorybox.dto;
+
+import com.memorybox.constant.QCategory;
+import com.memorybox.entity.QueBundle;
+import lombok.Getter;
+import lombok.Setter;
+import org.modelmapper.ModelMapper;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+public class QueBundleFormDto {
+    private Long id;
+
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
+    private String queBundleNm;
+
+    private String qCategory;
+
+    private static ModelMapper modelMapper = new ModelMapper();
+
+    public QueBundle createQueBundle() {
+
+        return modelMapper.map(this, QueBundle.class); // ItemFormDto -> Item 연결
+    }
+
+    public static QueBundleFormDto of(QueBundle queBundle) {
+
+        return modelMapper.map(queBundle, QueBundleFormDto.class); // Item -> ItemFormDto 연결
+    }
+}
