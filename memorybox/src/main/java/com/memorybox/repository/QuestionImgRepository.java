4fc88d4aba12c1b0e79dbfca78c32916c676f8f8
1J-Choi <chldnjswp110@naver.com>
2022-08-26 14:50:35 +0900

    문제 삭제 기능 관련 수정

--- memorybox/src/main/java/com/memorybox/repository/QuestionImgRepository.java
+++ memorybox/src/main/java/com/memorybox/repository/QuestionImgRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionImgRepository extends JpaRepository<QuestionImg,Long> {
     String findImgUrlByQuestionId(Long questionId);
+
+    QuestionImg findByQuestionId(Long questionId);
 }
